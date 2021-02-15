f9e10247a31eb1c4a9f83949ef2443f69abd34b6
saroy <sayantan.roy@xyleminc.com>
2021-02-15 17:50:52 +0530

    Add test for rest of the operations

--- src/test/java/com/ebi/app/controller/PersonRestControllerTest.java
+++ src/test/java/com/ebi/app/controller/PersonRestControllerTest.java
@@ -4,8 +4,11 @@ import com.ebi.app.EnableMockSecurityTestConfig;
 import com.ebi.app.TestDataUtils;
 import com.ebi.app.WithMockSecurity;
 import com.ebi.app.model.web.request.PersonDTO;
+import com.ebi.app.model.web.response.ErrorResponse;
+import com.ebi.app.model.web.response.Persons;
 import com.ebi.app.model.web.response.WebApiResponse;
 import com.ebi.app.service.PersonService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,6 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
@@ -29,6 +33,7 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -92,15 +97,104 @@ class PersonRestControllerTest {
     }
 
     @Test
-    void getPersons() {
+    @WithMockSecurity(scopes = "ebi:user")
+    void getPersons() throws Exception {
+        var persons = Persons.builder()
+                .person(personResponseDTOS).currentPage(0)
+                .totalItems(2L).totalPages(1).build();
+        when(personService.findAll(anyInt(), anyInt())).thenReturn(persons);
+        var expectedResponse = WebApiResponse.success(persons);
+
+        var requestBuilder = MockMvcRequestBuilders
+                .get("/person")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON);
+        var result = mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(expectedResponse)))
+                .andReturn();
+        var responseContent = result.getResponse().getContentAsString();
+        assertNotNull(responseContent);
+        JSONAssert.assertEquals(objectMapper.writeValueAsString(expectedResponse), responseContent, false);
     }
 
     @Test
-    void savePerson() {
+    @WithMockSecurity(username = "admin", password = "admin", scopes = {"ebi:admin"})
+    void savePerson() throws Exception {
+        var personResponseDTO = personResponseDTOS.get(0);
+        var personID = UUID.randomUUID().toString();
+        personResponseDTO.setId(personID);
+        var personRequestDTO = personDTOS.get(0);
+        when(personService.save(personRequestDTO)).thenReturn(personResponseDTO);
+        var expectedResponse = WebApiResponse.success(personResponseDTO);
+
+        var requestBuilder = MockMvcRequestBuilders
+                .post("/person")
+                .content(objectMapper.writeValueAsString(personRequestDTO))
+                .with(csrf().asHeader())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON);
+        var result = mockMvc.perform(requestBuilder)
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(content().json(objectMapper.writeValueAsString(expectedResponse)))
+                .andReturn();
+        var responseContent = result.getResponse().getContentAsString();
+        assertNotNull(responseContent);
+        JSONAssert.assertEquals(objectMapper.writeValueAsString(expectedResponse), responseContent, false);
     }
 
     @Test
-    void updatePerson() {
+    @WithMockSecurity(scopes = {"ebi:user"})
+    void savePersonNotAuthorized() throws Exception {
+        var personResponseDTO = personResponseDTOS.get(0);
+        var personID = UUID.randomUUID().toString();
+        personResponseDTO.setId(personID);
+        var personRequestDTO = personDTOS.get(0);
+        when(personService.save(personRequestDTO)).thenReturn(personResponseDTO);
+        var expectedResponse = WebApiResponse.error(ErrorResponse.builder()
+                        .message("Access is denied").statusCode(HttpStatus.UNAUTHORIZED).build());
+
+        var requestBuilder = MockMvcRequestBuilders
+                .post("/person")
+                .content(objectMapper.writeValueAsString(personRequestDTO))
+                .with(csrf().asHeader())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON);
+        var result = mockMvc.perform(requestBuilder)
+                .andDo(print())
+                .andExpect(status().isUnauthorized())
+                .andExpect(content().json(objectMapper.writeValueAsString(expectedResponse)))
+                .andReturn();
+        var responseContent = result.getResponse().getContentAsString();
+        assertNotNull(responseContent);
+        JSONAssert.assertEquals(objectMapper.writeValueAsString(expectedResponse), responseContent, false);
+    }
+
+    @Test
+    @WithMockSecurity(username = "admin", password = "admin", scopes = {"ebi:admin"})
+    void updatePerson() throws Exception {
+        var personResponseDTO = personResponseDTOS.get(0);
+        var personID = UUID.randomUUID().toString();
+        personResponseDTO.setId(personID);
+        var personRequestDTO = personDTOS.get(0);
+        when(personService.update(personRequestDTO, personID)).thenReturn(Optional.of(personResponseDTO));
+        var expectedResponse = WebApiResponse.success(personResponseDTO);
+
+        var requestBuilder = MockMvcRequestBuilders
+                .put("/person/{id}", personID)
+                .content(objectMapper.writeValueAsString(personRequestDTO))
+                .with(csrf().asHeader())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON);
+        var result = mockMvc.perform(requestBuilder)
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(expectedResponse)))
+                .andReturn();
+        var responseContent = result.getResponse().getContentAsString();
+        assertNotNull(responseContent);
+        JSONAssert.assertEquals(objectMapper.writeValueAsString(expectedResponse), responseContent, false);
     }
 
     @Test
